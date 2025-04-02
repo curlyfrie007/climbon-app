1d7c6eb46d191cad6e356aaf50f7536606656a2b
root <root@srv777631.hstgr.cloud>
2025-04-02 07:44:13 +0000

    changed redirect to a tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
+"use client"
 import Image from "next/image";
-import { useRouter } from "next/router";
+//import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  router.push("/event/RocklandsCup2025");
+  //const router = useRouter();
+  //router.push("/event/RocklandsCup2025");
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        
+        <a href="/event/RocklandsCup2025">Rocklands Cup 2025</a>
       </main>
       
     </div>
